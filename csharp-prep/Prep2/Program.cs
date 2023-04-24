ed6c1807ee300e796342ed00787d3c7daf7152e6
definitely-a-human <mike.makes.robots.24123@gmail.com>
2023-04-23 21:06:02 -0600

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        // Determine letter grade
+        string letterGrade = "";
+
+        if (grade >= 90) 
+        {
+            letterGrade = "A";
+        } 
+        else if (grade >= 80) 
+        {
+            letterGrade = "B";
+        } 
+        else if (grade >= 70) 
+        {
+            letterGrade = "C";
+        } 
+        else if (grade >= 60) 
+        {
+            letterGrade = "D";
+        } 
+        else 
+        {
+            letterGrade = "F";
+        }
+
+        // Print out letter grade
+        Console.WriteLine($"Your letter grade is: {letterGrade}");
+
+        // Check if the student passed
+        if (grade >= 70) 
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        } 
+        else 
+        {
+            Console.WriteLine("Sorry, you did not pass the course. Keep trying!");
+        }
     }
 }
