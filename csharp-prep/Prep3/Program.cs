1bd4571fe6ae1e4ccbbc120edd85da83dd3ab5a5
definitely-a-human <mike.makes.robots.24123@gmail.com>
2023-04-30 16:17:24 -0600

    Prep Three Number Guessing Game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,46 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+class Program {
+  static void Main(string[] args) {
+    // Generate a random number between 1 and 100
+    Random random = new Random();
+    int magicNumber = random.Next(1, 101);
+
+    int guessCount = 0; // keep track of how many guesses the user has made
+
+    // Loop until the user guesses the magic number
+    while (true) {
+      Console.Write("What is your guess? ");
+      int guess = int.Parse(Console.ReadLine());
+
+      guessCount++;
+
+      if (guess < magicNumber) {
+        Console.WriteLine("Higher");
+
+      } else if (guess > magicNumber) {
+        Console.WriteLine("Lower");
+
+      } else {
+        Console.WriteLine("You guessed it in " + guessCount + " guesses!");
+
+        Console.Write("Do you want to play again? (yes/no) ");
+        string answer = Console.ReadLine().ToLower();
+
+      if (answer == "yes") {
+        // Generate a new magic number and reset the guess count
+        magicNumber = random.Next(1, 101);
+        guessCount = 0;
+
+        // Play the game again
+        continue;
+      } else if (answer == "no") {
+        // End the program
+        Console.WriteLine("Thanks for playing!");
+        
+        break;
+      }
+      }
     }
+  }
 }
